17f29f30c9dfeab01f6124202f40fe1608b0c540
Joey Salakha <joeysalakha81@gmail.com>
2025-08-02 17:12:59 +0000

    Please add this snippet
    <script async src="https://pagead2.googlesyndic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
@@ -20,6 +21,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9370081174240595"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen">
         <ThemeProvider
